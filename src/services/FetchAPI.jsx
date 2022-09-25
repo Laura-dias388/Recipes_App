15991302ec1ed3bb76f8623919ebfe3323410036
LAURICEA VITAL DE TOLEDO DIAS <104790453+Laura-dias388@users.noreply.github.com>
2022-09-24 22:00:50 -0300

    Requisito 10

---
+++ src/services/FetchAPI.jsx
@@ -0,0 +1,7 @@
+/* export const fetchQuestion = async (token) => {
+  const URL = `https://opentdb.com/api.php?amount=5&token=${token}`;
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+};
+ */
