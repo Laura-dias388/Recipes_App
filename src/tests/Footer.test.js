809b527f2838d4c80ee772dc307802a3994ef906
Rubens Deola <rdeola@gmail.com>
2022-09-27 16:58:39 -0300

    Testes do Footer

--- src/tests/Footer.test.js
+++ src/tests/Footer.test.js
@@ -2,19 +2,24 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './helpers/renderWithRouter';
-import Meals from '../pages/Meals';
+import Footer from '../components/Footer';
 
-describe('Testes para a tela de Login', () => {
-  test('Se o Footer possui os botôes corretos', () => {
+describe('Testes para o componente Footer', () => {
+  test('Se no Footer a clicar em Drinks vai para a página', () => {
     // Este arquivo pode ser modificado ou deletado sem problemas
-    renderWithRouter(<Meals />, '/meals');
-
-    const mealsBtn = screen.queryByTestId('meals-bottom-btn');
-    expect(mealsBtn).toBeInTheDocument();
-    userEvent.click(mealsBtn);
+    renderWithRouter(<Footer />, '/meals');
 
     const drinksBtn = screen.queryByTestId('drinks-bottom-btn');
     expect(drinksBtn).toBeInTheDocument();
     userEvent.click(drinksBtn);
+  });
+
+  test('Se no Footer ao clicar em Meals vai para a página', () => {
+    // Este arquivo pode ser modificado ou deletado sem problemas
+    renderWithRouter(<Footer />, '/drinks');
+
+    const mealsBtn = screen.queryByTestId('meals-bottom-btn');
+    expect(mealsBtn).toBeInTheDocument();
+    userEvent.click(mealsBtn);
   });
 });
