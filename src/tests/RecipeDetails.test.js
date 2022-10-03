e998dfad50b90fe60dc5d98f870980ce1457edbb
Rubens Deola <rdeola@gmail.com>
2022-10-03 09:43:20 -0300

    RecipeDetails.teste at 80%

--- src/tests/RecipeDetails.test.js
+++ src/tests/RecipeDetails.test.js
@@ -5,7 +5,7 @@ import App from '../App';
 import fetch from '../../cypress/mocks/fetch';
 import renderWithRouter from './helpers/renderWithRouter';
 
-const DONE_RECIPE = [{
+const DONE_RECIPE = {
   id: '52771',
   type: 'meal',
   nationality: 'Italian',
@@ -15,7 +15,15 @@ const DONE_RECIPE = [{
   image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
   doneDate: '01/10/2022',
   tags: 'Pasta,Curry',
-}];
+};
+const IN_PROGRESS_RECIPE = {
+  drinks: {
+    178319: [' Hpnotiq - 2 oz', ' Pineapple Juice - 1 oz', ' Banana Liqueur - 1 oz'],
+  },
+  meals: {
+    52771: ['ALGO'],
+  },
+};
 
 describe('Testa a página do componente RecipeDetails', () => {
   beforeEach(() => {
@@ -23,12 +31,14 @@ describe('Testa a página do componente RecipeDetails', () => {
   });
 
   test('Render page with drink recipe 178319 and test elements', async () => {
+    localStorage.setItem('inProgressRecipes', JSON.stringify(IN_PROGRESS_RECIPE));
     renderWithRouter(<App />, '/drinks/178319');
     expect(await screen.findByTestId('recipe-title')).toHaveTextContent('Aquamarine');
   });
 
   test('Render page with meal recipe 52771 and test elements', async () => {
     localStorage.setItem('doneRecipes', JSON.stringify(DONE_RECIPE));
+    localStorage.setItem('inProgressRecipes', JSON.stringify(IN_PROGRESS_RECIPE));
     renderWithRouter(<App />, '/meals/52771');
 
     expect(await screen.findByTestId('recipe-title')).toHaveTextContent('Spicy Arrabiata Penne');
