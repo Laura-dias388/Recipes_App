b178f922ed6b22114558803003b29c225cc143b8
Rubens Deola <rdeola@gmail.com>
2022-10-03 08:43:36 -0300

    não foi dado push nessa. Voltar depois

---
+++ src/tests/RecipeDetails.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import fetch from '../../cypress/mocks/fetch';
+import renderWithRouter from './helpers/renderWithRouter';
+
+const DONE_RECIPE = [{
+  id: '52771',
+  type: 'meal',
+  nationality: 'Italian',
+  category: 'Vegetarian',
+  alcoholicOrNot: '',
+  name: 'Spicy Arrabiata Penne',
+  image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+  doneDate: '01/10/2022',
+  tags: 'Pasta,Curry',
+}];
+
+describe('Testa a página do componente RecipeDetails', () => {
+  beforeEach(() => {
+    jest.spyOn(global, 'fetch').mockImplementation(fetch); // Obrigado Mentoria :)
+  });
+
+  test('Render page with drink recipe 178319 and test elements', async () => {
+    renderWithRouter(<App />, '/drinks/178319');
+    expect(await screen.findByTestId('recipe-title')).toHaveTextContent('Aquamarine');
+  });
+
+  test('Render page with meal recipe 52771 and test elements', async () => {
+    localStorage.setItem('doneRecipes', JSON.stringify(DONE_RECIPE));
+    renderWithRouter(<App />, '/meals/52771');
+
+    expect(await screen.findByTestId('recipe-title')).toHaveTextContent('Spicy Arrabiata Penne');
+  });
+});
