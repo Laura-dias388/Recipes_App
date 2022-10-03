70463e4cfe3f423beda01a5c1aabe059db8b9c9c
Chrystian Strummiello <chrystian.strummiello@gmail.com>
2022-10-03 01:24:24 -0300

    requisito 58

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,39 @@
 import React from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import useLocalStorage from '../hooks/useLocalStorage';
 
 function Profile() {
+  // const [email, setEmail] = useState([]);
+
+  const [user] = useLocalStorage('user', {});
+
   return (
     <div>
       <Header />
+      <div>
+        <p data-testid="profile-email">{user.email}</p>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+        >
+          Done Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+        >
+          Favorite Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+        >
+          Logout
+        </button>
+      </div>
       <Footer />
     </div>
   );
 }
-
 export default Profile;
