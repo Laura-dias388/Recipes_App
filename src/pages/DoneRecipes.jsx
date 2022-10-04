a0754874ad5ddff3150638680648f5f042cdae63
LAURICEA VITAL DE TOLEDO DIAS <104790453+Laura-dias388@users.noreply.github.com>
2022-10-04 17:10:56 -0300

    teste

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import Header from '../components/Header';
 
-function DoneRecipes() {
+export default function DoneRecipes() {
   return (
     <div>
+      DoneRecipes
       <Header />
     </div>
   );
 }
-
-export default DoneRecipes;
